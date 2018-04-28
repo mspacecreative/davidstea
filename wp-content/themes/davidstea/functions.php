57e543a1aff47199050c52e6a652b3d9977fb22c
Matt <matt@mspacecreative.com>
2018-04-28 15:27:06 -0300

    c

--- wp-content/themes/davidstea/functions.php
+++ wp-content/themes/davidstea/functions.php
@@ -3,7 +3,7 @@
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     
-    wp_register_script('object-fit', get_stylesheet_directory_uri() . 'js/fitie.js', array('jquery'), null, true);
+    wp_register_script('object-fit', get_stylesheet_directory_uri() . 'js/object-fit-polyfill.js', array('jquery'), null, true);
     wp_enqueue_script('object-fit');
     
     wp_register_script('my-scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true);
